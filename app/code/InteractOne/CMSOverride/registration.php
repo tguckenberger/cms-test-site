34d35110987f91f3a482b18414356a25e09b5ac0
Tristan Guckenberger <tguckenberger@interactone.com>
2018-04-21 13:20:50 -0400

    Module Structure

---
+++ app/code/InteractOne/CMSOverride/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'InteractOne_CMSOverride',
+    __DIR__
+);
+
+?>
